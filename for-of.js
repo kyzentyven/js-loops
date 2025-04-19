66c166461916af44fc8ef2f7fe7fb14faf114729
kyzentyven <kyzentyven@gmail.com>
2025-04-19 22:56:22 +0600

    Added basic concepts of for loop in JavaScript.

---
+++ for-of.js
@@ -0,0 +1,13 @@
+const numbers = [1, 22, 35, 4, 17, 8, 12, 5, 7, 11, 9];
+
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+}
+
+const fruits = ['Apple', 'Banana', 'Orange', 'Cherry'];
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
