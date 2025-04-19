430f36b0cf09ed2844756c261c13b2509e44b1ac
kyzentyven <kyzentyven@gmail.com>
2025-04-20 01:10:02 +0600

    Added basic concepts of break in JavaScript.

---
+++ break.js
@@ -0,0 +1,17 @@
+// Using for loop
+for(let i = 0; i < 15; i++){
+    console.log(i);
+    if(i >= 10){
+        break;
+    }
+}
+
+// Using while loop
+let i = 50;
+while(i > 25){
+    if(i < 50){
+        break;
+    }
+    console.log(i);
+    i--;
+}
